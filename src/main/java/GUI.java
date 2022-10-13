a934d7ea48ca52f78e2d4a86f1cc700fa5fecd13
WillG <will.g.garner@googlemail.com>
2022-10-13 11:24:27 +0100

    minor gui changes

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,32 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import static java.awt.BorderLayout.*;
 
 public class GUI extends JFrame {
-    static int rows = 2;
-    static int cols = 3;
-    JButton restartButton;
-    JLabel label2 = new JLabel("");
-    boolean restart;
-
-
-
-
+    private JButton restartButton;
+    private final JLabel winLabel = new JLabel("");
+    
         public GUI(int x, int y) {
-
             startGame(x, y);
-
         }
 
         public void startGame(int x, int y) {
-            Board test = new Board(x, y);
-            test.createBoard();
-            //test.printBoard();
+            Board gameBoard = new Board(x, y);
+            gameBoard.createBoard();
 
             Container panel = getContentPane();
 
@@ -40,26 +29,23 @@ public class GUI extends JFrame {
             label.setHorizontalAlignment(JLabel.CENTER);
             labelContainer.add(label, PAGE_START);
 
-            label2.setText("");
-            label2.setHorizontalAlignment(JLabel.CENTER);
-            labelContainer.add(label2, PAGE_END);
+            winLabel.setText("");
+            winLabel.setHorizontalAlignment(JLabel.CENTER);
+            labelContainer.add(winLabel, PAGE_END);
 
             panel.add(labelContainer, PAGE_START);
 
             restartButton = new JButton("Restart");
-            restartButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    resetGame();
-                    startGame(x,y);
-                }
+            restartButton.addActionListener(e -> {
+                resetGame();
+                startGame(x,y);
             });
 
 
-            JButton[][] buttonArray = new JButton[test.getX()][test.getX()];
+            JButton[][] buttonArray = new JButton[gameBoard.getX()][gameBoard.getX()];
 
-            for (int i = 0; i < test.getX(); i++) {
-                for (int j = 0; j < test.getX(); j++) {
+            for (int i = 0; i < gameBoard.getX(); i++) {
+                for (int j = 0; j < gameBoard.getX(); j++) {
                     buttonArray[i][j] = new JButton(".");
                 }
             }
@@ -67,7 +53,7 @@ public class GUI extends JFrame {
             int j = 0;
             int k = 0;
 
-            for (int i = 0; i < (test.getX() * test.getX()); i++) {
+            for (int i = 0; i < (gameBoard.getX() * gameBoard.getX()); i++) {
                 int l = j;
                 int m = k;
                 JButton button = buttonArray[l][m];
@@ -75,24 +61,22 @@ public class GUI extends JFrame {
                 button.setForeground(Color.WHITE);
 
                 button.addMouseListener(new MouseListener() {
+
                     @Override
                     public void mouseClicked(MouseEvent e) {
-
                     }
-
-
 
                     @Override
                     public void mousePressed(MouseEvent e) {
 
                         if (SwingUtilities.isLeftMouseButton(e)) {
-                            hit(button, l, m, test, buttonArray);
+                            hit(button, l, m, gameBoard, buttonArray);
                         }
 
                         if (SwingUtilities.isRightMouseButton(e)) {
-                            test.setFlag(test.getBoard()[l][m]);
-                            if (!test.getBoard()[l][m].isClear()) {
-                                if (test.getBoard()[l][m].isFlagged()) {
+                            gameBoard.setFlag(gameBoard.getBoard()[l][m]);
+                            if (!gameBoard.getBoard()[l][m].isClear()) {
+                                if (gameBoard.getBoard()[l][m].isFlagged()) {
                                     button.setBackground(Color.YELLOW);
                                     button.setForeground(Color.WHITE);
                                     button.setText("F");
@@ -101,41 +85,35 @@ public class GUI extends JFrame {
                                     button.setForeground(Color.WHITE);
                                     button.setText(".");
                                 }
-                                //test.printBoard();
-                                checkVictory(buttonArray, test);
+                                checkVictory(buttonArray, gameBoard);
                             }
                         }
 
                         if (SwingUtilities.isMiddleMouseButton(e)) {
-                            //test.bulk(test.getBoard()[l][m].getX(), test.getBoard()[l][m].getX());
-                            bulkhit(l, m, buttonArray, test);
+                            bulkHit(l, m, buttonArray, gameBoard);
                         }
 
                     }
 
                     @Override
                     public void mouseReleased(MouseEvent e) {
-
                     }
 
                     @Override
                     public void mouseEntered(MouseEvent e) {
-
                     }
 
                     @Override
                     public void mouseExited(MouseEvent e) {
-
                     }
                 });
                 buttonPane.add(button);
 
                 k++;
-                if (k == test.getX()) {
+                if (k == gameBoard.getX()) {
                     k = 0;
                     j++;
                 }
-
             }
 
             panel.add(buttonPane, CENTER);
@@ -150,25 +128,24 @@ public class GUI extends JFrame {
 
     // TODO Add some restart check
 
-    public void hit(JButton button, int l, int m, Board test, JButton[][] buttonArray){
+    public void hit(JButton button, int l, int m, Board gameBoard, JButton[][] buttonArray){
         button.setBackground(Color.GRAY);
         button.setForeground(Color.BLACK);
         //button.setEnabled(false);
-        test.hit(l, m, false);
-        if (test.getBoard()[l][m].checkBomb()) {
+        gameBoard.hit(l, m, false);
+        if (gameBoard.getBoard()[l][m].checkBomb()) {
             button.setBackground(Color.RED);
             button.setForeground(Color.BLACK);
-        } else if (test.getBoard()[l][m].getTouching() == 0) {
+        } else if (gameBoard.getBoard()[l][m].getTouching() == 0) {
             button.setText("");
-            recursion(l, m, test.getX(), buttonArray, test);
+            recursion(l, m, buttonArray, gameBoard);
         } else {
-            button.setText(Integer.toString(test.getBoard()[l][m].getTouching()));
+            button.setText(Integer.toString(gameBoard.getBoard()[l][m].getTouching()));
         }
-        //test.printBoard();
-        checkVictory(buttonArray, test);
+        checkVictory(buttonArray, gameBoard);
     }
 
-        public void recursion(int l, int m, int x, JButton[][] buttonArray, Board test){
+        public void recursion(int l, int m, JButton[][] buttonArray, Board test){
             for(int i = -1; i <= 1; i++) {
                 for(int j = -1; j <= 1; j++) {
                     if(l+j >= 0 && m+i < test.getX() && m+i >= 0 && j+l < test.getX()) {
@@ -177,7 +154,7 @@ public class GUI extends JFrame {
                             buttonArray[l + j][m + i].setBackground(Color.GRAY);
                             buttonArray[l + j][m + i].setForeground(Color.BLACK);
                             //buttonArray[l + j][m + i].setEnabled(false);
-                            recursion(l + j, m + i, x, buttonArray, test);
+                            recursion(l + j, m + i, buttonArray, test);
                         } else if (!buttonArray[l+j][m+i].getText().equals("") && test.getBoard()[l+j][m+i].getTouching() > 0 && !test.getBoard()[l+j][m+i].checkBomb() && !test.getBoard()[l+j][m+i].isFlagged()) {
                             buttonArray[l + j][m + i].setBackground(Color.GRAY);
                             buttonArray[l + j][m + i].setForeground(Color.BLACK);
@@ -200,33 +177,32 @@ public class GUI extends JFrame {
             }
         }
 
-        public void checkVictory(JButton[][] buttonArray, Board test) {
-            test.checkVictory();
-            if(test.getGameOver()){
-                if(test.victoryState() == 1) {
-                    label2.setText("YOU WON! YOU FLAGGED THE MINES");
-                } else if (test.victoryState() == 2) {
-                    label2.setText("YOU WON! YOU CLEARED ALL THE TILES");
+        public void checkVictory(JButton[][] buttonArray, Board gameBoard) {
+            gameBoard.checkVictory();
+            if(gameBoard.getGameOver()){
+                if(gameBoard.victoryState() == 1) {
+                    winLabel.setText("YOU WON! YOU FLAGGED THE MINES");
+                } else if (gameBoard.victoryState() == 2) {
+                    winLabel.setText("YOU WON! YOU CLEARED ALL THE TILES");
                 } else {
-                    test.flagAllBombs();
-                    flagAllBombs(buttonArray, test);
-                    label2.setText("YOU LOST!");
+                    gameBoard.flagAllBombs();
+                    flagAllBombs(buttonArray, gameBoard);
+                    winLabel.setText("YOU LOST!");
                 }
                 restartButton.setVisible(true);
             }
         }
 
 
-        public void bulkhit(int l, int m, JButton[][] buttonArray, Board test){
+        public void bulkHit(int l, int m, JButton[][] buttonArray, Board gameBoard){
             for(int i = -1; i <= 1; i++) {
                 for(int j = -1; j <= 1; j++) {
-                    if(l+j >= 0 && l+j < test.getX() && m+i >= 0 && m+i < test.getX()) {
-                        if(!buttonArray[l+j][m+i].getText().equals("F") && !test.getBoard()[l+j][m+i].isClear()) {
-                            hit(buttonArray[l+j][m+i], l+j, m+i, test, buttonArray);
+                    if(l+j >= 0 && l+j < gameBoard.getX() && m+i >= 0 && m+i < gameBoard.getX()) {
+                        if(!buttonArray[l+j][m+i].getText().equals("F") && !gameBoard.getBoard()[l+j][m+i].isClear()) {
+                            hit(buttonArray[l+j][m+i], l+j, m+i, gameBoard, buttonArray);
                         }
-                        //test.printBoard();
 
-                        checkVictory(buttonArray, test);
+                        checkVictory(buttonArray, gameBoard);
                     }
                 }
             }
