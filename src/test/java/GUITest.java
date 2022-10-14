252ea1000ddff0a373d886ed4bdbcabef566ba5e
WillG <will.g.garner@googlemail.com>
2022-10-14 14:14:41 +0100

    Testing Jacoco

---
+++ src/test/java/GUITest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.*;
+
+public class GUITest {
+
+    GUI testGUI = new GUI(1,2);
+    GUI testGUI2 = new GUI(5, 5);
+
+    @Test
+    public void testStartGame() {
+
+    }
+
+}
